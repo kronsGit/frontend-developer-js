71d675004b0fa477f74117ec146cbdeedc00cc81
kronsGit <109983643+kronsGit@users.noreply.github.com>
2024-05-03 14:03:38 -0600

    more  code

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const menuEmail = document.querySelector('.navbar-email'); //Seleccionamos la el elemento con el que vamos a trabajar
 const desktopMenu = document.querySelector('.desktop-menu'); //Seleccionamos el elemento que le agregaremos la clase
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 
 //Creamos la llamada del evento
 
 menuEmail.addEventListener('click', toggleDesktopMenu); 
+menuHamIcon.addEventListener('click', toggleMobilepMenu); 
+
 //Con esta linea se escucha el evento click y se activa la funcion que crearemos
 
 function toggleDesktopMenu(){
-    console.log('Click')
+  
     desktopMenu.classList.toggle('inactive');
     //Aqui le estamos diciendo al elemento que agrego o se quite la clase "inactive"
 }
 
-console.log('Js Funcionando');
+
+//Esto es para quitar y poner una clase con JS
+function toggleMobilepMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
