aff02e4f60d28a6b3c9b5537be332996f7b70825
kronsGit <109983643+kronsGit@users.noreply.github.com>
2024-05-20 21:59:57 -0600

    Code
    More dode

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ const desktopMenu = document.querySelector('.desktop-menu'); //Seleccionamos el 
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
+const asideProduct = document.querySelector('.product-detail');
 
 
 //Creamos la llamada del evento
 
 menuEmail.addEventListener('click', toggleDesktopMenu); 
 menuHamIcon.addEventListener('click', toggleMobilepMenu); 
+menuCarIcon.addEventListener('click', toggleCarIconMenu)
 
 //Con esta linea se escucha el evento click y se activa la funcion que crearemos
 
@@ -18,9 +21,13 @@ function toggleDesktopMenu(){
     //Aqui le estamos diciendo al elemento que agrego o se quite la clase "inactive"
 }
 
-
 //Esto es para quitar y poner una clase con JS
 function toggleMobilepMenu(){
+    asideProduct.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
 }
 
+function toggleCarIconMenu(){
+    mobileMenu.classList.add('inactive');
+    asideProduct.classList.toggle('inactive');
+} 
